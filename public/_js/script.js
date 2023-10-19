766b0f0f79134dfeb4961b2a50a3ae6dda83bce3
Carlos Alves <carlosalves369m@hotmail.com>
2023-10-19 18:10:26 -0300

    Update templates

--- public/_js/script.js
+++ public/_js/script.js
@@ -14,6 +14,41 @@ var select = document.getElementById("select"),
                  select.insertBefore(option,select.lastChild);
              }
 
+// Estados para colocar no Select
+var especialidade = document.getElementById("especialidade"),
+                     especialidades = [
+                        "Direito penal",
+                        "Direito de propriedade intelectual",
+                        "Direito do consumidor",
+                        "Direito ambiental",
+                        "Direito internacional",
+                        "Direito empresarial",
+                        "Direito trabalhista",
+                        "Direito tributário",
+                        "Direito desportivo",
+                        "Direito aeronáutico",
+                        "Direito agrário",
+                        "Direito eleitoral",
+                        "Direito previdenciário",
+                        "Direito de Família",
+                        "Lei de Trânsito",
+                        "Direito Bancário",
+                        "Direito administrativo",
+                        "Direito médico",
+                        "Direito de sucessão",
+                        "Direito digital",
+                        "Direito contratual",
+                     ];
+             
+             for(var i = 0; i < especialidades.length; i++)
+             {
+                 var option = document.createElement("option"),
+                     txt = document.createTextNode(especialidades[i]);
+                 option.appendChild(txt);
+                 option.setAttribute("value",especialidades[i]);
+                 especialidade.insertBefore(option,especialidade.lastChild);
+             }
+
 //Função de Redirecionamento
 function btnBusca(){
     var link =  document.getElementById("busca").value;
