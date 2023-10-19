8712b2795375883c60dda6f338e7cf1bfcf43a4c
Carlos Alves <carlosalves369m@hotmail.com>
2023-10-19 18:09:22 -0300

    Update database

---
+++ database/db.sql
@@ -0,0 +1,35 @@
+CREATE TABLE 'registro' (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    nome VARCHAR,
+    identificador VARCHAR, -- Número de identificação profissional 213066/RJ
+    tel VARCHAR,
+    email VARCHAR,
+    estado VARCHAR,
+    especialidade VARCHAR,
+    biografia VARCHAR,
+    website VARCHAR
+)
+
+/**
+Direito penal
+Direito de propriedade intelectual
+Direito do consumidor
+Direito ambiental
+Direito internacional
+Direito empresarial
+Direito trabalhista
+Direito tributário
+Direito desportivo
+Direito aeronáutico
+Direito agrário
+Direito eleitorial
+Direito previdenciario
+Direito de Família
+Lei de Trânsito
+Direito Bancário
+Direito administrativo
+Direito médico
+Direito de sucessão
+Direito digital
+Direito contratual
+**/
